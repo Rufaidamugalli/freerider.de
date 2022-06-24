859e968e9278ba45a3ccabb2d0e30e6704385f91
Rufaidamugalli <rufaida@upday.com>
2022-06-24 11:16:10 +0200

    japa data

--- src/main/java/de/freerider/app/Application.java
+++ src/main/java/de/freerider/app/Application.java
@@ -4,14 +4,17 @@ import de.freerider.datamodel.Customer;
 import de.freerider.repository.CustomerRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories( basePackages = { "de.freerider.repository" } ) // tell Spring where to scan for @Entity's
+@EntityScan( basePackages = { "de.freerider.datamodel" } )
+// tell Spring where to scan for @Components, @Controllers, @Services
+@ComponentScan( basePackages = {"de.freerider.restapi", "de.freerider.repository"} )
 @SpringBootApplication
-@ComponentScan(
-        basePackages = {"de.freerider.restapi", "de.freerider.repository"}
-)
 public class Application {
     // Spring auto‐wires reference to CustomerRepository instance
     private final CustomerRepository customerRepository;
